a37807cb5f924161ce512e33158785d0dca570ef
okdavekk <okdavekk@gmail.com>
2022-07-24 11:28:33 -0400

    Have Header, Nav, Project, and Footer sections with styling.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import React from "react";
-import Counter from "./components/Counter";
+import React from 'react';
+import Navbar from './components/Navbar';
+import Header from './components/Header';
+import Project from './components/Project';
+import Footer from './components/Footer';
 
 function App() {
-  return <Counter />;
+  return (
+    <div>
+      <Header />
+      <Navbar />
+      <Project />
+      <Project />
+      <Project />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/Footer.css';
+
+function Footer() {
+    return (
+      <header className="footer">
+        <h1>Made by DK</h1>
+      </header>
+    );
+  }
+  
+  export default Footer;
